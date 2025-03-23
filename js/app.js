60af143e699e78f3c0138cde4677ddb103eab47e
Jeferson <gemesulva1964@gmail.com>
2025-03-23 20:33:18 -0300

    add

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 "use strict";
+var switcher = document.querySelector(".btn");
 switcher.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
 
   var className = document.body.className;
   if (className == "light-theme") {
     this.textContent = "Dark";
-  } else {
+  } else if (className == "dark-theme"){
     this.textContent = "Light";
+  }else{
+    this.textContent = "Dark";
   }
   console.log('current class name:' + className);
-  
-});
+}
+);
